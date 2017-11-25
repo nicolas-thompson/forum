38a0782bf5994ff1a5062e6cbc7e87706a409ec5
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-25 10:01:04 +0000

    Adding belongsTo() relationship between Reply and User

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -3,17 +3,16 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ReplyTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    function it_has_an_owner ()
     {
-        $this->assertTrue(true);
+        $reply = factory('App\Reply')->create();
+        $this->assertInstanceOf('App\User', $reply->owner);
     }
 }
