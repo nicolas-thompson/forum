0e069c14c362631df27dc0df12aa3d4fd7e476d8
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-25 07:51:58 +0000

    Renamed ThreadTests to ReadThreadTests

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -9,9 +9,7 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
    
-    /**
-     * @test
-     */
+    /** @test */
     public function a_user_can_browse_all_threads()
     {
         $thread = factory('App\Thread')->create();
@@ -20,14 +18,20 @@ class ThreadsTest extends TestCase
         $response->assertSee($thread->title);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function a_user_can_browse_a_single_threads()
     {
         $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
+    }
+
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        // given we have a thread
+        // and that thread includes replies
+        // when we visit a thread page 
+        // then we should see the replies
     }
 }
