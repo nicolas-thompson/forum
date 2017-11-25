b3a9e4fed57f29f9e177b235b24d9f668ed1cba7
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-25 09:28:57 +0000

    Adding replies to threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -12,27 +12,29 @@ class ThreadsTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->thread = factory('App\Thread')->create();        
+        $this->thread = factory('App\Thread')->create();
     }
    
     /** @test */
-    public function a_user_can_browse_all_threads()
+    public function a_user_can_view_all_threads()
     {
-        $response = $this->get('/threads');
-        $response->assertSee($this->thread->title);
+        $this->get('/threads')->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_browse_a_single_threads()
     {
-        $response = $this->get('/threads/' . $this->thread->id);
-        $response->assertSee($this->thread->title);
+        $this->get('/threads/' . $this->thread->id)->assertSee($this->thread->title);
     }
 
+    /** @test */    
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
-        // given we have a thread
-        // and that thread includes replies
+        $reply = factory('App\Reply')
+        ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+        ->assertSee($reply->body);        
         // when we visit a thread page 
         // then we should see the replies
     }
