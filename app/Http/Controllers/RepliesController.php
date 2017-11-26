01b1d89612e74c53d0239680bcc133382faa78b6
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-26 07:38:02 +0000

    Added Store method to Replies Controller

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
-    //
+    public function store(Thread $thread)
+    {
+        $thread->addReply([
+            'user_id'   => auth()-id(),
+            'body'      => request('body'),
+        ]);
+    
+        return back();
+    }
 }
