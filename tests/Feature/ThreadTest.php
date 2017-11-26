c1548f85dc71f9ab5ce68d34528a41eea013dbf6
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-26 07:43:20 +0000

    Added a thread can add a reply test

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -5,21 +5,39 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class Thread extends TestCase
+class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
+
+    protected $thread;
     
+    function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();        
+    }
+
     /** @test */
     function a_thread_has_replies()
     {
-        $thread = factory('App\Thread')->create();
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     /** @test */
     function a_thread_has_a_creator()
     {
-        $thread = factory('App\Thread')->create();
-        $this->assertInstanceOf('App\User', $thread->creator);        
+
+        $this->assertInstanceOf('App\User', $this->thread->creator);        
+    }
+
+    /** @test */
+    function a_thread_can_add_a_reply()
+    {
+        $this->thread->addReply([
+            'body'      => 'Foobar',
+            'user_id'   => 1 
+        ]);
+
+        $this->assertCount(1, $this->thread->replies);
     }
 }
