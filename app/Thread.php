66fc0be2ab2f580dd41f071d4108e08f07678475
Nicolas Thompson <mail@nicolasthompson.com>
2017-11-26 07:39:13 +0000

    Added  property to prevent mass assignmant exception

--- app/Thread.php
+++ app/Thread.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected $guarded = [];
+
+    
     public function path()
     {
         return '/threads/' . $this->id;
