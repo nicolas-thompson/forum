810411fb36e7710cfdec69ceb978b477f0a4ab6a
Nicolas Thompson <nic@thinktmb.com>
2017-12-14 18:30:21 +0000

    A user can publish a thread

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
 {
+
+    /**
+     * ThreadsController constructer.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -36,7 +44,13 @@ class ThreadsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $thread = Thread::create([
+            'user_id' => request('user_id'),
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        return redirect($thread->path());
     }
 
     /**
