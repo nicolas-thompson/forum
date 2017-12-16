061bd135100600695cce20a37ed33edbee092cb4
Nicolas Thompson <mail@nicolasthompson.com>
2017-12-16 10:03:34 +0000

    Add channel

--- app/Thread.php
+++ app/Thread.php
@@ -24,6 +24,11 @@ class Thread extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+    
     public function addReply($reply)
     {
         $this->replies()->create($reply);
--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -15,8 +15,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Thread::class, function (Faker $faker) {
     return [
-        'user_id'=> function () {
+        'user_id' => function () {
             return factory('App\User')->create()->id;
+        },
+        'channel_id' =>  function () {
+            return factory('App\Channel')->create()->id;
         },
         'title' => $faker->sentence,
         'body'  => $faker->paragraph
