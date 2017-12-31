9daabb0a17f74d4e628ef96bb5b8a70b6dfcc4e8
Nicolas Thompson <mail@nicolasthompson.com>
2017-12-31 11:13:12 +0000

    Pass channel to threads index for filtering & make phpunit prettier

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Channel;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -20,9 +21,18 @@ class ThreadsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($channelSlug = null)
     {
-        $threads = Thread::latest()->get();
+        if($channelSlug){
+            $channelId = Channel::where('slug', $channelSlug)->first()->id;
+            $threads = Thread::where('channel_id', $channelId)->latest()->get();
+
+        } else {
+
+            $threads = Thread::latest()->get();
+            
+        }
+
         return view('threads.index',  compact('threads'));
     }
 
