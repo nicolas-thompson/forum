7335bc54ba5309864ac7159b366de151333820ff
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-11 19:36:11 +0000

    Add favourites controller

---
+++ app/Http/Controllers/FavouritesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavouritesController extends Controller
+{
+    public function store(Reply $reply)
+    {
+        
+    }
+}
