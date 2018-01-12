4cb644bfb863c61b69d801d694a3cc8ec7fa89f4
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-12 19:05:37 +0000

    Add favourite button to reply

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,17 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{$reply->created_at->diffForHumans()}}...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{$reply->created_at->diffForHumans()}}...
+            </h5>
+            <div>
+                <form action="">
+                    <button type="submit" class="btn btn-default">Favourite</button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="panel-body">
         {{ $reply->body }}
