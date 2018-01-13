976f93cdcfed12d6fbaf5f5afb0bea2278a07182
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-13 17:50:40 +0000

    Move methods to do with favourting a reply into a trait

--- app/Reply.php
+++ app/Reply.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favouritable;
+
     protected $guarded = [];
 
     protected $with = ['owner', 'favourites'];
@@ -14,28 +16,4 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
-
-    public function favourites()
-    {
-        return $this->morphMany(Favourite::class, 'favourited');
-    }
-
-    public function favourite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-        if (! $this->favourites()->where($attributes)->exists()) {
-            return $this->favourites()->create($attributes);
-        }
-    }
-
-    public function isFavourited()
-    {
-        return !! $this->favourites->where('user_id', auth()->id())->count();
-    }
-
-    public function getFavouritesCountAttribute()
-    {
-        return $this->favourites->count();
-    }
-
 }
