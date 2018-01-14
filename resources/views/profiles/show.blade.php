a76b0355d04a2fee9f49e30be60013455a10cbb6
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-14 20:12:26 +0000

    Show user profile and add test

---
+++ resources/views/profiles/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="page-header">
+        <h1>
+            {{$profileUser->name}}
+            <small>Since {{$profileUser->created_at->diffForHumans()}}</small>
+        </h1>
+    </div>
+</div>
+@endsection
---
+++ tests/Feature/ProfilesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ProfilesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_has_a_profile()
+    {
+        $user = create('App\User');
+        $this->get("/profiles/{$user->name}")
+            ->assertSee(e($user->name));
+    }
+}
