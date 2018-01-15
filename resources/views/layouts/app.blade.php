efd8de61da3b6c84947d008e003632308e3f4fa5
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-15 22:32:05 +0000

    Move nav into a partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,78 +40,7 @@
 						{{ config('app.name', 'Laravel') }}
 					</a>
 				</div>
-
-				<div class="collapse navbar-collapse" id="app-navbar-collapse">
-					<!-- Left Side Of Navbar -->
-					<ul class="nav navbar-nav">
-						<li class="dropdown">
-							<a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-								Browse
-								<span class="caret"></span>
-							</a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="/threads">All Threads</a>
-                                </li>
-                                @if (auth()->check())
-								<li>
-									<a href="/threads?by={{ auth()->user()->name }}">My Threads</a>
-                                </li>
-								@endif
-								<li><a href="/threads?popular=1">Popular Threads</a></li>
-							</ul>
-						</li>
-						<li>
-							<a href="/threads/create">New Thread</a>
-						</li>
-						<li class="dropdown">
-							<a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-								Channels
-								<span class="caret"></span>
-							</a>
-							<ul class="dropdown-menu">
-								@foreach ( $channels as $channel)
-								<li>
-									<a href="/threads/{{$channel->slug}}">{{$channel->name}}</a>
-								</li>
-								@endforeach
-							</ul>
-					</ul>
-					</li>
-					</ul>
-					<!-- Right Side Of Navbar -->
-					<ul class="nav navbar-nav navbar-right">
-						<!-- Authentication Links -->
-						@guest
-						<li>
-							<a href="{{ route('login') }}">Login</a>
-						</li>
-						<li>
-							<a href="{{ route('register') }}">Register</a>
-						</li>
-						@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-								{{ Auth::user()->name }}
-								<span class="caret"></span>
-							</a>
-
-							<ul class="dropdown-menu">
-								<li>
-									<a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-										Logout
-									</a>
-
-									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-										{{ csrf_field() }}
-									</form>
-								</li>
-							</ul>
-						</li>
-						@endguest
-					</ul>
-				</div>
+				@include('layouts.nav')
 			</div>
 		</nav>
 
