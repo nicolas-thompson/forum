c1bd8faf98b7c7636dc46b19c3cd1feb897dd5e8
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-16 21:55:32 +0000

    Add message for if there are no threads for a channel

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            @foreach ($threads as $thread)
+            @forelse ($threads as $thread)
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <div class="level">
@@ -19,7 +19,9 @@
                         <div class="body">{{ $thread->body }}</div>
                     </div>
                 </div>
-            @endforeach            
+            @empty
+                <p>There are no relevant results at this time.</p>    
+            @endforelse            
         </div>
     </div>
 </div>
