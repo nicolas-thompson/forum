d05485648df1ae5c588c07a461ec3313b95ecda3
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-17 23:10:53 +0000

    Assert morph relationship between activity and subject is working

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Activity;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -21,5 +22,9 @@ class ActivityTest extends TestCase
             'subject_id' => $thread->id,
             'subject_type' => 'App\Thread'
         ]);
+
+        $activity = Activity::first();
+
+        $this->assertEquals($activity->subject->id, $thread->id);
     }
 }
