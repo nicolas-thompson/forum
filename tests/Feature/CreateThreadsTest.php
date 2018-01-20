91ebe129394d58b621f7a630e293626fc0ba723d
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-20 15:33:03 +0000

    Tidy up authorized_users_can_delete_threads by asserting the activities table has no records after delete

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use Tests\TestCase;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -64,14 +65,8 @@ class CreateThreadsTest extends TestCase
         $response->assertStatus(204);
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
-        $this->assertDatabaseMissing('activities', [
-            'subject_id' => $thread->id,
-            'subject_type' => get_class($thread)
-        ]);
-        $this->assertDatabaseMissing('activities', [
-            'subject_id' => $reply->id,
-            'subject_type' => get_class($reply)
-        ]);
+       
+        $this->assertEquals(0, Activity::count());
     }
 
 
