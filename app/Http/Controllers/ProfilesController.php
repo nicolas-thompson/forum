acbd61c23232a2c14c6bb47625412e0aa4450042
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-20 07:25:43 +0000

    Group activities by date

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,9 +8,10 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        $activities = $user->activity()->latest()->with('subject')->get();
+        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
 
-        return $activities;
         return view('profiles.show', [
             'profileUser' => $user,
             'activities' => $activities
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
-
 @section('content')
-
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -11,10 +9,11 @@
                         <small>Since {{$profileUser->created_at->diffForHumans()}}</small>
                     </h1>
             </div>
-
-
-            @foreach($activities as $activity)
-                @include ("profiles.activities.{$activity->type}")
+            @foreach($activities as $date => $activity)
+                <h3 class="page-header">{{ $date }}</h3>
+                @foreach ($activity as $record)
+                    @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                @endforeach
             @endforeach
                 {{--  {{$threads->links()}}  --}}   
         </div>
