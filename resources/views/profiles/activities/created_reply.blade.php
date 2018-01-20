5f0c5dc07df08fcfec6ce0d659cfbb815d8c070a
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-20 07:55:54 +0000

    Use components to reduce duplication

--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -1,13 +1,8 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <div class="level">
-            <span class="flex">
-                {{ $profileUser->name }} replied to 
-                <a href="{{ $activity->subject->thread->path() }}"> {{ $activity->subject->thread->title }} </a>
-            </span>
-        </div>
-    </div>
-    <div class="panel-body">
+@component('profiles.activities.activity')
+    @slot('heading')
+    {{ $profileUser->name }} replied to <a href="{{ $activity->subject->thread->path() }}"> {{ $activity->subject->thread->title }} </a>
+    @endslot
+    @slot('body')
         {{ $activity->subject->body }}
-    </div>
-</div>
+    @endslot
+@endcomponent
--- resources/views/profiles/activities/created_thread.blade.php
+++ resources/views/profiles/activities/created_thread.blade.php
@@ -1,13 +1,8 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-            <div class="level">
-                <span class="flex">
-                    {{ $profileUser->name }} published a 
-                    <a href="{{ $activity->subject->path() }}">{{ $activity->subject->title }}</a>
-                </span>
-            </div>
-    </div>
-    <div class="panel-body">
+@component('profiles.activities.activity')
+    @slot('heading')
+        {{ $profileUser->name }} published a <a href="{{ $activity->subject->path() }}">{{ $activity->subject->title }}</a>
+    @endslot
+    @slot('body')
         {{ $activity->subject->body }}
-    </div>  
-</div>
+    @endslot
+@endcomponent
