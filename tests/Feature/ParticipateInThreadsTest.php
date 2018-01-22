63ee62f75b979557fecf4ae7e3d62c3e199c08d8
Nicolas Thompson <mail@nicolasthompson.com>
2018-01-22 20:49:48 +0000

    Delete a_reply_requires_a_body test

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -11,18 +11,6 @@ class ParticipateInThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-    
-    /** @test */
-    function a_reply_requires_a_body()
-    {
-        $this->withExceptionHandling()->signIn();
-        
-        $thread = create('App\Thread');
-        $reply = make('App\Reply', ['body' => null]);
-        
-        $this->post($thread->path() . '/replies', $reply->toArray())
-        ->assertSessionHasErrors('body');
-    }
     /** @test */
     function unauthorized_users_cannot_delete_replies()
     {
