498949c3113f32df0a89b5038db0e4ea19572746
Nicolas Thompson <nic@thinktmb.com>
2018-02-05 16:59:14 +0000

    Add test a_reply_requires_a_body

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -37,6 +37,16 @@ class ParticipateInThreadsTest extends TestCase
     }
 
     /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+             ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
     function unauthorized_users_cannot_delete_replies()
     {
         $this->withExceptionHandling();
