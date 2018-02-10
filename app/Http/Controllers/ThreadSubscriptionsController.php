2cee7656d57d7a95e37b7da9faef4205929edd3e
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-10 20:23:01 +0000

    New store method on ThreadSubscriptionsController

--- app/Http/Controllers/ThreadSubscriptionsController.php
+++ app/Http/Controllers/ThreadSubscriptionsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use Illuminate\Http\Request;
 
 class ThreadSubscriptionsController extends Controller
 {
-    //
+    public function store($channelId, Thread $thread)
+    {
+        $thread->subscribe();
+    }
 }
