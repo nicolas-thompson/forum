d4a9c23fe15e57edc0d5deacbc148106a425c4b0
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-12 21:47:53 +0000

    New getIsSubscribedToAttribute method on Thread model

--- app/Thread.php
+++ app/Thread.php
@@ -75,4 +75,11 @@ class Thread extends Model
         return $this->hasMany(ThreadSubscription::class);
     }
 
+    public function getIsSubscribedToAttribute()
+    {
+        return $this->subscriptions()
+        ->where('user_id', auth()->id())
+        ->exists();
+    }
+
 }
