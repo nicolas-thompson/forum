f51f927e691538f8a5d73de52e599fb2adb68f4b
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-15 06:30:27 +0000

    Check a_user_can_subscribe_to_threads

--- tests/Feature/SubscribeToThreadTest.php
+++ tests/Feature/SubscribeToThreadTest.php
@@ -25,15 +25,16 @@ class SubscribeToThreadsTest extends TestCase
         // And the user subscribes...
         $this->post($thread->path() .  '/subscriptions');
 
-        $this->assertCount(0, auth()->user()->notifications);
+        $this->assertCount(1, $thread->fresh()->subscriptions);
+        // $this->assertCount(0, auth()->user()->notifications);
         
-        // Then each time a reply is left...
-        $thread->addReply([
-            'user_id' => auth()->id(),
-            'body' => 'Some reply'
-        ]);
+        // // Then each time a reply is left...
+        // $thread->addReply([
+        //     'user_id' => auth()->id(),
+        //     'body' => 'Some reply'
+        // ]);
 
-        $this->assertCount(1, auth()->user()->fresh()->notifications);
+        // $this->assertCount(1, auth()->user()->fresh()->notifications);
         // A notification should be prepared for the user.
         // $this->assertCount(1, $thread->subscriptions);
     }
