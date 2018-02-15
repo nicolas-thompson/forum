e9714a08facf9a8b5350a045040a03276bd6ca15
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-15 07:15:31 +0000

    Use higher order collections to notify each subscription that a reply has been left

--- app/ThreadSubscription.php
+++ app/ThreadSubscription.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-// use App\User;
 use Illuminate\Database\Eloquent\Model;
+use App\Notifications\ThreadWasUpdated;
 
 class ThreadSubscription extends Model
 {
@@ -12,5 +12,10 @@ class ThreadSubscription extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function notify($reply)
+    {
+        $this->user->notify(new ThreadWasUpdated($this, $reply));
     }
 }
