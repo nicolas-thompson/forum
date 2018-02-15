b648b3a41431ff3b0f3e41b07081fe042963d70f
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-15 06:40:57 +0000

    New a_notification_is_prepared_when_a_subscribed_thread_receives_new_reply test

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -3,18 +3,27 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class NotificationsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_notification_is_prepared_when_a_subscribed_thread_receives_new_reply()
     {
-        $this->assertTrue(true);
+        $this->signIn();
+
+        $thread = create('App\Thread')->subscribe();
+        $this->assertCount(0, auth()->user()->notifications);
+        
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply'
+        ]);
+
+        $this->assertCount(1, auth()->user()->fresh()->notifications);
+        // A notification should be prepared for the user.
+        $this->assertCount(1, $thread->subscriptions);
     }
 }
--- tests/Feature/SubscribeToThreadTest.php
+++ tests/Feature/SubscribeToThreadTest.php
@@ -19,38 +19,18 @@ class SubscribeToThreadsTest extends TestCase
     public function a_user_can_subscribe_to_threads()
     {
         $this->signIn();
-
-        // Given we have a thread...
         $thread = create('App\Thread');
-        // And the user subscribes...
         $this->post($thread->path() .  '/subscriptions');
-
         $this->assertCount(1, $thread->fresh()->subscriptions);
-        // $this->assertCount(0, auth()->user()->notifications);
-        
-        // // Then each time a reply is left...
-        // $thread->addReply([
-        //     'user_id' => auth()->id(),
-        //     'body' => 'Some reply'
-        // ]);
-
-        // $this->assertCount(1, auth()->user()->fresh()->notifications);
-        // A notification should be prepared for the user.
-        // $this->assertCount(1, $thread->subscriptions);
     }
 
     /** @test */
     public function a_user_can_unsubscribe_from_threads()
     {
         $this->signIn();
-
-        // Given we have a thread...
         $thread = create('App\Thread');
-
         $thread->subscribe();
-
         $this->delete($thread->path() .  '/subscriptions');
-
         $this->assertCount(0, $thread->subscriptions);
         
     }
