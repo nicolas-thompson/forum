9da22e06bcf23211c908bdc3176c18bca715c2f6
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-15 20:24:41 +0000

    Move signIn into setup method on class

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -9,11 +9,15 @@ class NotificationsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->signIn();        
+    }
+
     /** @test */
     function a_notification_is_prepared_when_a_subscribed_thread_receives_new_reply_that_is_not_by_the_current_user()
     {
-        $this->signIn();
-
         $thread = create('App\Thread')->subscribe();
         $this->assertCount(0, auth()->user()->notifications);
         
@@ -35,27 +39,16 @@ class NotificationsTest extends TestCase
     /** @test */
     function a_user_can_fetch_their_unread_notifications()
     {
-        $this->signIn();    
-        
-        $thread = create('App\Thread')->subscribe();
-        
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'Some reply'
-        ]);  
-
+        create(DataBaseNotification::class);
+ 
         $user = auth()->user();
         
-        $response = $this->getJson("/profiles/{$user->name}/notifications")->json();
-
-        $this->assertCount(1, $response);
+        $this->assertCount(1, $this->getJson("/profiles/" .  auth()->user()->name . "/notifications")->json());
     }
 
     /** @test */
     function a_user_can_mark_a_notification_as_read()
     {
-        $this->signIn();
-
         $thread = create('App\Thread')->subscribe();
 
         $thread->addReply([
