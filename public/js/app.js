1c1d70bf027fcb608ec8ee81a65073b55003c604
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-19 22:09:33 +0000

    Remove prevent from default on Unread notifications link

--- public/js/app.js
+++ public/js/app.js
@@ -60933,7 +60933,6 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
       },
       on: {
         "click": function($event) {
-          $event.preventDefault();
           _vm.markAsRead(notification)
         }
       }
--- resources/assets/js/components/UserNotifications.vue
+++ resources/assets/js/components/UserNotifications.vue
@@ -6,7 +6,7 @@
         </a>
         <ul class="dropdown-menu">
             <li v-for="notification in notifications">
-                <a :href="notification.data.link" v-text="notification.data.message" @click.prevent="markAsRead(notification)"></a>
+                <a :href="notification.data.link" v-text="notification.data.message" @click="markAsRead(notification)"></a>
             </li>
         </ul>
     </li>
