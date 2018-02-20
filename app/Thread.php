297b6a5cceb30fd75dee82ecab6fd3a1ea68b4c5
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-20 09:35:30 +0000

    Create new event stubs for ThreadHasNewReply

--- app/Thread.php
+++ app/Thread.php
@@ -53,9 +53,11 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions
-            ->where('user_id', '!=', $reply->user_id) 
-            ->each->notify($reply);
+        event(new ThreadHasNewReply);
+        
+        // $this->subscriptions
+        //     ->where('user_id', '!=', $reply->user_id) 
+        //     ->each->notify($reply);
 
         return $reply;
     }
