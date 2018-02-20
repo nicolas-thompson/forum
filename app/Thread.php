6ae785130ed1a3a9d8ff366d4669a682cffd515a
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-20 09:28:24 +0000

    Use where clause instead of filter on subsciptions collection in addReply method on Thread model

--- app/Thread.php
+++ app/Thread.php
@@ -54,10 +54,8 @@ class Thread extends Model
         $reply = $this->replies()->create($reply);
 
         $this->subscriptions
-            ->filter(function ($sub) use ($reply) {
-            return $sub->user_id != $reply->user_id;
-        })
-        ->each->notify($reply);
+            ->where('user_id', '!=', $reply->user_id) 
+            ->each->notify($reply);
 
         return $reply;
     }
