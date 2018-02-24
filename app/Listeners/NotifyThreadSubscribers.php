d4908b7ae068a7fdf2168f11ca002281f60322da
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-24 14:37:17 +0000

    Remove unused constuctor

--- app/Listeners/NotifyThreadSubscribers.php
+++ app/Listeners/NotifyThreadSubscribers.php
@@ -9,16 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class NotifyThreadSubscribers
 {
     /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      *
      * @param  ThreadHasNewReply  $event
