79ab100f543b08da7ee5712cbbc31b0c4ba4f2fb
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-25 09:29:19 +0000

    New detectKeyHeldDown method

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -8,14 +8,26 @@ use Tests\TestCase;
 class SpamTest extends TestCase
 {
     /** @test */
-    function it_validates_spam()
+    function it_checks_for_invalid_keywords()
     {
+        // Invalid keywords
+        
         $spam = new Spam();
-
+        
         $this->assertFalse($spam->detect('Innocent reply here.'));
-
+        
         $this->expectException('Exception');
-
+        
         $spam->detect('yahoo customer support');
+    }
+    
+    /** @test */
+    function it_check_for_any_key_being_held_down()
+    {
+        // Key held down
+        $spam = new Spam();
+        $this->expectException('Exception');        
+        $spam->detect('Hello world aaaaaaaaaaa');
+        
     }
 }
