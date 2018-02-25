8007249db83b09da44dd723a381c540957321cb9
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-25 08:24:55 +0000

    New it_validates_spam test

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,9 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Spam;
 use Tests\TestCase;
 
 class SpamTest extends TestCase
 {
+    /** @test */
+    function it_validates_spam()
+    {
+        $spam = new Spam();
 
+        $this->assertFalse($spam->detect('Innocent reply here.'));
+    }
 }
