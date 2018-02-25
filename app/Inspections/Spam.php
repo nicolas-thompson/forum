580aba890e2dc852e9e793c313e9e7c497aa3ccc
Nicolas Thompson <mail@nicolasthompson.com>
2018-02-25 09:57:06 +0000

    Move invalid keyword inpsection into it's own class

--- app/Inspections/Spam.php
+++ app/Inspections/Spam.php
@@ -4,26 +4,21 @@ namespace App\Inspections;
 
 class Spam 
 {
+    protected $inspections = [
+        InvalidKeywords::class
+    ];
 
     public function detect($body)
     {
-        $this->detectInvalidKeywords($body);
+        foreach($this->inspections as $inspection) {
+        
+            app($inspection)->detect($body);
+        }
+
+        // $this->detectInvalidKeywords($body);
         $this->detectKeyHeldDown($body);
 
         return false;
-    }
-
-    protected function detectInvalidKeywords($body)
-    {
-        $invalidKeywords = [
-            'yahoo customer support'
-        ];
-
-        foreach($invalidKeywords as $keyword) {
-            if(stripos($body, $keyword) !== false) {
-                throw new \Exception('Your reply contains spam.');
-            }
-        }
     }
 
     protected function detectKeyHeldDown($body) 
