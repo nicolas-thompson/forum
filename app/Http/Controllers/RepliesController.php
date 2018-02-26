7cd9c67ac2d3d02dcabc0c97a35216b939f0f4ab
nicolas-thompson <mail@nicolasthompson.com>
2018-02-26 17:20:22 +0000

    Remove commented out code

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -29,7 +29,6 @@ class RepliesController extends Controller
     {
         try {
             request()->validate(['body' => 'required|spamfree']);
-            // $this->validate(request(), ['body' => 'required|spamfree']);
 
             $reply = $thread->addReply([
                 'user_id'   => auth()->id(),
