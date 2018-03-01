0ee1c6436c6ef5790f91a247a510d7150ff6f4ec
nicolas-thompson <mail@nicolasthompson.com>
2018-03-01 16:56:08 +0000

    New mentioned_users_in test

--- tests/Feature/MentionUsersTest.php
+++ tests/Feature/MentionUsersTest.php
@@ -5,16 +5,30 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class MentionUsersTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+    
+    /** @test */
+    function mentioned_users_in()
     {
-        $this->assertTrue(true);
+        // Given I have a user, JohnDoe, who is signed in.
+        $john = create('App\User', ['name' => 'JohnDoe']);
+        $this->signIn($john);
+        // And another user, Jane Doe.
+        $jane = create('App\User', ['name' => 'JaneDoe']);
+        
+
+        // If we have a thread.
+        $thread = create('App\Thread');
+        // And JohnDoe replies and mentions @JaneDoe.
+        $reply = make('App\Reply', [
+            'body' => '@JaneDoe look at this.'
+        ]);
+        $this->json('post', $thread->path() . '/replies', $reply->toArray());
+        // Then, JaneDoe should be notified.
+        $this->assertCount(1, $jane->notifications);
     }
 }
