41b5b74786e298b8a8f0f5b48c8dbb1411f98e42
Nicolas Thompson <mail@nicolasthompson.com>
2018-03-07 21:47:49 +0000

    SHow creator of thread against thread

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -7,17 +7,22 @@
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <div class="level">
-                            <h4 class="flex">
-                                <a href="{{ $thread->path() }}">
-                                    @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                    <strong>
-                                        {{$thread->title}}
-                                    </strong>
-                                    @else
-                                        {{$thread->title}}                                    
-                                    @endif
-                                </a>
-                            </h4>
+                            <div class="flex">
+                                <h4>
+                                    <a href="{{ $thread->path() }}">
+                                        @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                                        <strong>
+                                            {{$thread->title}}
+                                        </strong>
+                                        @else
+                                            {{$thread->title}}                                    
+                                        @endif
+                                    </a>
+                                </h4>
+                                <h5>
+                                    Posted By: <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
+                                </h5>
+                            </div>
                             <a href="{{ $thread->path() }}">
                                 {{$thread->replies_count}} {{str_plural('reply', $thread->replies_count)}}
                             </a>
