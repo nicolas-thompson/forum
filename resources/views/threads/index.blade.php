2189626f6a8903a70b9bb66ae179d99d5072b024
nicolas-thompson <mail@nicolasthompson.com>
2018-03-09 15:08:16 +0000

    Remove rogue semi-colon

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            @include('threads._list');
+            @include('threads._list')
             {{ $threads->render() }}            
         </div>
     </div>
