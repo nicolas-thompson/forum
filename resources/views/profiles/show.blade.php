0ca85c06a337f50c98e4509e7e1637c0e0a67236
Nicolas Thompson <mail@nicolasthompson.com>
2018-12-25 09:50:48 +0000

    Replace avatar html form with avatar vue component

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,25 +4,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="page-header">
-                    <h1>
-                        {{$profileUser->name}}
-                    </h1>
-            
-                    @can ('update', $profileUser)
-
-                        <form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
-                            
-                            {{ csrf_field() }}
-                            
-                            <input type="file" name="avatar">
-                            
-                            <button type="submit" class="btn btn-primary">Add Avatar</button>
-
-                        </form>
-                        
-                    @endcan
-
-                    <img src="{{ asset($profileUser->avatar()) }}" alt="avatar" width="50" height="50">
+                    
+            <avatar-form :user="{{ $profileUser }}"></avatar-form>
 
             </div>
             @forelse($activities as $date => $activity)
