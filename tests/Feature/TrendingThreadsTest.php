54534a0677419b155dbc25c9c2581f3f9014cf06
Nicolas Thompson <mail@nicolasthompson.com>
2018-12-29 06:16:53 +0000

    Deligate fetching testing trending threads to trending class

--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -23,16 +23,17 @@ class TrendingThreadsTest extends TestCase
     /** @test **/
     public function it_increments_a_test_score_each_time_it_is_read()
     {
-        
         $this->assertEmpty($this->trending->get());
+
         $thread = create('App\Thread');
+
         $this->call('GET', $thread->path());
 
-        $trending = Redis::zrevrange('testing_trending_threads', 0, -1);
+        $trending = $this->trending->get();
 
         $this->assertCount(1, $trending);
 
-        $this->assertEquals($thread->title, json_decode($trending[0])->title);
+        $this->assertEquals($thread->title, $trending[0]->title);
 
     }
 }
