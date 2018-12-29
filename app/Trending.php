b3c8476a73f228059bf06f89cf3e475ae60497cd
Nicolas Thompson <mail@nicolasthompson.com>
2018-12-29 05:58:48 +0000

    Use redis key based on environment

--- app/Trending.php
+++ app/Trending.php
@@ -8,14 +8,19 @@ class Trending
 {
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange('trending_threads', 0, 4));
+        return array_map('json_decode', Redis::zrevrange($this->cachekey(), 0, 4));
     }
-
+    
     public function push($thread)
     {
-        Redis::zincrby('trending_threads', 1, json_encode([
+        Redis::zincrby($this->cachekey(), 1, json_encode([
             'title' => $thread->title,
             'path' => $thread->path()
         ]));
+    }
+
+    public function cachekey()
+    {
+        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
     }
 }
