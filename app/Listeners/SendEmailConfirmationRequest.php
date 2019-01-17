64baf859b34ab042e0993671e0430e52e08d16a3
Nicolas Thompson <mail@nicolasthompson.com>
2019-01-17 20:47:28 +0000

    add send email confirmation

---
+++ app/Listeners/SendEmailConfirmationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Providers\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\PleaseConfirmYourEmail;
+
+class SendEmailConfirmationRequest
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        Mail::to($event->user)->send(new PleaseConfirmYourEmail());
+    }
+}
