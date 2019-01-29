821a4b91178d4b7794a1948a0ff61c9021112bca
Nicolas Thompson <mail@nicolasthompson.com>
2019-01-29 20:07:18 +0000

    complie js

--- public/js/app.js
+++ public/js/app.js
@@ -59248,6 +59248,9 @@ module.exports = {
         var prop = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'user_id';
 
         return model[prop] === user.id;
+    },
+    isAdmin: function isAdmin() {
+        return ['Nicolas Thompson'].includes(user.name);
     }
 };
 
@@ -59985,12 +59988,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 /* harmony default export */ __webpack_exports__["default"] = ({
 
-    props: ['initialRepliesCount'],
+    props: ['dataRepliesCount', 'dataLocked'],
     components: { Replies: __WEBPACK_IMPORTED_MODULE_0__components_Replies_vue___default.a, SubscribeButton: __WEBPACK_IMPORTED_MODULE_1__components_SubscribeButton_vue___default.a },
 
     data: function data() {
         return {
-            repliesCount: this.initialRepliesCount
+            repliesCount: this.dataRepliesCount,
+            locked: this.dataLocked
         };
     }
 });
@@ -60046,6 +60050,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue__ = __webpack_require__(182);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__NewReply_vue__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__mixins_collections__ = __webpack_require__(187);
+//
+//
+//
 //
 //
 //
@@ -62608,7 +62615,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
     on: {
       "changed": _vm.fetch
     }
-  }), _vm._v(" "), _c('new-reply', {
+  }), _vm._v(" "), (_vm.$parent.locked) ? _c('p', [_vm._v("\n        This thread has been locked. No more repliess are allowed.\n    ")]) : _c('new-reply', {
     on: {
       "created": _vm.add
     }
